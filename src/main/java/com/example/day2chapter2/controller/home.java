0b0431efd26253461289e2180ad130c7f91b587f
piyush7983 <154682036+piyush7983@users.noreply.github.com>
2026-01-19 15:07:43 +0530

    added more objects

--- src/main/java/com/example/day2chapter2/controller/home.java
+++ src/main/java/com/example/day2chapter2/controller/home.java
@@ -1,5 +1,6 @@
 package com.example.day2chapter2.controller;
-
+import java.util.*;
+import com.example.day2chapter2.model.StudentModel;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,7 +8,19 @@ import org.springframework.web.bind.annotation.RestController;
 public class home {
 
     @GetMapping("about")
-    public String about(){
-        return "this is about";
+    public ArrayList<StudentModel> getStudent(){
+
+        ArrayList<StudentModel> al=new ArrayList<>();
+        StudentModel student =new StudentModel(1,"piyush","piyush@gmail.com");
+        al.add(student);
+
+        al.add(new StudentModel(2,"manoj","666@gmail.com"));
+        al.add(new StudentModel(3,"tushar","aaaaa@gmail.com"));
+        al.add(new StudentModel(4,"tanmay","-1@gmail.com"));
+        al.add(new StudentModel(5,"rahul","2019@gmail.com"));
+
+
+        return al;
     }
+
 }
